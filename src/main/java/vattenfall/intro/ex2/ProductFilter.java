59d80884d22bf52abe1b8e4a2897ffff2642f40b
Raweos <kosa.095@gmail.com>
2019-03-31 15:10:07 +0200

    Refactor

--- src/main/java/vattenfall/intro/ex2/ProductFilter.java
+++ src/main/java/vattenfall/intro/ex2/ProductFilter.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.function.BiFunction;
 
 class ProductFilter {
-    static BiFunction<Product, String, Boolean> checkCategory = (product, category) -> product.getCategory().equals(category)
-            && product.getPrice().equals(BigDecimal.TEN);
+    static BiFunction<Product, String, Boolean> checkCategory = (product, s) -> false;
 
 }
